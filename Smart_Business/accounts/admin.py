184d1f61904aa2c86d2f05cc84fb5ef5db8492fe
sudi6362758958 <sudeepsudegsd1234@gmail.com>
2025-12-16 18:21:59 +0530

    Deleting project

--- Smart_Business/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Business
-
-@admin.register(Business)
-class BusinessAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owner', 'email', 'is_approved', 'created_at')
-    list_filter = ('is_approved',)
-
